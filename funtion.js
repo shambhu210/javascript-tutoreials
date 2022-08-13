b0fe93d2e90f6cacd613e9d5aa18eda14effd9b9
shambhu210 <48592457+shambhu210@users.noreply.github.com>
2022-08-13 15:34:26 +0530

    new tutorial function

---
+++ funtion.js
@@ -0,0 +1,84 @@
+// ONLY FOR DEBUGGING POURPOSE , DO NOT USE IN PRODUCTION
+console.log("WORKING")
+
+const demo = false
+
+// IF ELSE STATEMENT
+
+// if(demo = "DEV"){
+//     console.log(demo)
+// } 
+// ERROR BECAUSE CONDITION IS INVALID
+
+// SERIES OF CONDITIONAL OPERATORS
+// ! = NOT
+// && = and
+// == = equal
+// type of = value type
+// || = or 
+
+// LOGIC 
+
+// = : use with any logical operator
+// == : slighty match 
+// === : needs to match with exact same value
+
+
+const hello = 0 // iNT
+const demo2 = false
+
+const num1 = 5
+const num2 = 8
+
+// CONDITIONAL OPERATORS
+
+if (demo2){
+    console.log("STRING VALUE")
+
+} 
+else if (demo2 !== false){
+  
+       console.log("hello is true")
+}
+else if (demo2 !== false){
+  
+    console.log("hello is true 1")
+}
+
+else if (demo2 !== false){
+  
+    console.log("hello is true 2 ")
+}
+else if (demo2 !== true){
+  
+    console.log("hello is tru  3")
+}
+
+else{
+    //alert("FORMT IS EMPTY")
+    console.warn("THE VALUE FALSE")
+    if(hello == 0){
+        console.error("hello = 0")
+        console.table([
+            {
+            name:"hello1",
+            age:26
+        },
+        {
+            name:"hello2",
+            age:26
+        },
+
+    ]) // TABLE
+    }
+}
+
+var num = 3
+
+if (num%2==0){
+    
+    console.log("ODD")
+}
+else{
+    console.log("EVEN")
+}
